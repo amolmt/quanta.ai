f4756d10817611b6406652a3a2da43330d300370
Amol Tangade <tangadeamol@gmail.com>
2023-07-22 01:16:18 +0530

    add code generator and image generator models

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,15 +1,7 @@
-const AuthLayout = ({
-    children
-}: {
-    children: React.ReactNode
-}) => {
-    return ( 
-        <div className="flex flex-center justify-center h-full">
-            {
-                children
-            }
-        </div>
-     );
-}
- 
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-center justify-center h-full">{children}</div>
+  );
+};
+
 export default AuthLayout;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,11 @@
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
 
-const DashboardLayout = ({
-    children
-}: {
-    children: React.ReactNode
-}) => {
-    return ( 
-        <div className="h-full relative">
-            <div className="
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full relative">
+      <div
+        className="
             hidden 
             h-full 
             md:flex
@@ -17,16 +14,16 @@ const DashboardLayout = ({
             md:fixed
             md:inset-y-0
             z-[80]
-            bg-gray-900
-            ">
-                <Sidebar/>
-            </div>
-            <div className="md:pl-72">
-                <Navbar/>
-                {children}
-            </div>
-        </div>
-     );
-}
- 
+            bg-gray-900"
+      >
+        <Sidebar />
+      </div>
+      <div className="md:pl-72">
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+};
+
 export default DashboardLayout;
